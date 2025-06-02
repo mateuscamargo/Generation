6ab31bd7452153dac8f2074e6c85b39e21d0d040
mateuscamargo <mateuscamargolima@gmail.com>
2025-06-02 18:13:11 -0300

    "Projeto conta bancaria"

---
+++ Conta_Bancaria/Menu.ts
@@ -0,0 +1,117 @@
+import readlinesync = require("readline-sync");
+import { colors } from './src/util/Colors';
+
+export function main() {
+
+    let opcao: number;
+
+    while (true) {
+
+        console.log(colors.bg.black, colors.fg.yellow, 
+                    "*****************************************************");
+        console.log("                                                     ");
+        console.log(colors.bg.black, colors.fg.blue,"                BANCO DO BRAZIL COM Z                ");
+        console.log(colors.bg.black, colors.fg.yellow,"                                                     ");
+        console.log("*****************************************************");
+        console.log("                                                     ");
+        console.log("            1 - Criar Conta                          ");
+        console.log("            2 - Listar todas as Contas               ");
+        console.log("            3 - Buscar Conta por Numero              ");
+        console.log("            4 - Atualizar Dados da Conta             ");
+        console.log("            5 - Apagar Conta                         ");
+        console.log("            6 - Sacar                                ");
+        console.log("            7 - Depositar                            ");
+        console.log("            8 - Transferir valores entre Contas      ");
+        console.log("            9 - Sair                                 ");
+        console.log("                                                     ");
+        console.log("*****************************************************");
+        console.log("                                                     ", 
+        colors.reset);
+
+        console.log("Entre com a opção desejada: ");
+        opcao = readlinesync.questionInt("");
+
+        if (opcao == 9) {
+            console.log(colors.fg.blue, 
+                "\nBanco do Brazil com Z - O seu Futuro começa aqui!");
+            sobre();
+            console.log(colors.reset, "");
+            process.exit(0);
+        }
+
+        switch (opcao) {
+            case 1:
+                console.log(colors.fg.yellow, 
+                    "\n\nCriar Conta\n\n", colors.reset);
+                
+                keyPress()
+                break;
+            case 2:
+                console.log(colors.fg.yellow, 
+                    "\n\nListar todas as Contas\n\n", colors.reset);
+
+                keyPress()
+                break;
+            case 3:
+                console.log(colors.fg.yellow, 
+                    "\n\nConsultar dados da Conta - por número\n\n", colors.reset);
+
+                keyPress()
+                break;
+            case 4:
+                console.log(colors.fg.yellow, 
+                    "\n\nAtualizar dados da Conta\n\n", colors.reset);
+
+                keyPress()
+                break;
+            case 5:
+                console.log(colors.fg.yellow, 
+                    "\n\nApagar uma Conta\n\n", colors.reset);
+
+                keyPress()
+                break;
+            case 6:
+                console.log(colors.fg.yellow, 
+                    "\n\nSaque\n\n", colors.reset);
+
+                keyPress()
+                break;
+            case 7:
+                console.log(colors.fg.yellow, 
+                    "\n\nDepósito\n\n", colors.reset);
+
+                keyPress()
+                break;
+            case 8:
+                console.log(colors.fg.yellow, 
+                    "\n\nTransferência entre Contas\n\n", colors.reset);
+
+                keyPress()
+                break;
+            default:
+                console.log(colors.fg.yellow, 
+                    "\nOpção Inválida!\n", colors.reset);
+
+                keyPress()
+                break;
+        }
+    }
+
+}
+
+/* Função com os dados da pessoa desenvolvedora */
+function sobre(): void {
+    console.log(colors.fg.yellow,"\n*****************************************************");
+    console.log("Projeto Desenvolvido por: ");
+    console.log("Mateus Camargo - mateusc@genstudents.org");
+    console.log("github.com/mateuscamargo");
+    console.log("*****************************************************");
+}
+
+function keyPress(): void {
+    console.log(colors.reset, "");
+    console.log("\nPressione enter para continuar...");
+    readlinesync.prompt();
+}
+
+main();
